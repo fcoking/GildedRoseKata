6e16538669c1533326dc7c5d5753e78542d13db2
Francisco Javier Aceves Arreguin <aceves.pollo@gmail.com>
2015-10-16 16:18:00 -0500

    codigo acortado y funcional

--- GildedRoseKata/src/gildedrose/GildedRose.java
+++ GildedRoseKata/src/gildedrose/GildedRose.java
@@ -4,57 +4,59 @@ import java.util.List;
 
 public class GildedRose {
 	
+	public void decQuality(Item item){
+		item.setQuality(item.getQuality() - 1);
+	}
+	
+	public void incQuality(Item item){
+		item.setQuality(item.getQuality() + 1);
+	}
+	
 	public void updateQuality(List<Item> items) {
-		for (Item item : items) {
-
-			if (!item.getName().equals("Aged Brie") && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.getQuality() > 0) {
-					if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-						item.setQuality(item.getQuality() - 1);
-					}
+		for (Item item : items) {						
+///////////////////////////////////
+			if (!item.getName().equals("Aged Brie") && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert") && !item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+				if (item.getQuality() > 0) {					
+					decQuality(item);					
+				}
+				if(item.getName().equals("Conjured Mana Cake") && item.getQuality() > 0){
+					decQuality(item);
 				}
 			} else {
 				if (item.getQuality() < 50) {
 					item.setQuality(item.getQuality() + 1);
-
 					if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (item.getSellIn() < 11) {
-							if (item.getQuality() < 50) {
-								item.setQuality(item.getQuality() + 1);
-							}
+						if (item.getSellIn() < 11 && item.getQuality() < 50) {							
+							incQuality(item);							
 						}
-
-						if (item.getSellIn() < 6) {
-							if (item.getQuality() < 50) {
-								item.setQuality(item.getQuality() + 1);
-							}
+						if (item.getSellIn() < 6 && item.getQuality() < 50) {							
+							incQuality(item);							
 						}
 					}
 				}
 			}
-
 			if (item.getName() != "Sulfuras, Hand of Ragnaros") {
 				item.setSellIn(item.getSellIn() - 1);
 			}
-
 			if (item.getSellIn() < 0) {
 				if (item.getName() != "Aged Brie") {
 					if (item.getName() != "Backstage passes to a TAFKAL80ETC concert") {
-						if (item.getQuality() > 0) {
-							if (item.getName() != "Sulfuras, Hand of Ragnaros") {
-								item.setQuality(item.getQuality() - 1);
-							}
+						if (item.getQuality() > 0 && item.getQuality() != 80) {							
+							decQuality(item);							
+						}
+						if (item.getQuality() > 0 && item.getQuality() != 80 && item.getName().equals("Conjured Mana Cake") && item.getQuality() > 0) {							
+							decQuality(item);							
 						}
 					} else {
-						item.setQuality(item.getQuality() - item.getQuality());
+						item.setQuality(0);
 					}
 				} else {
 					if (item.getQuality() < 50) {
-						item.setQuality(item.getQuality() + 1);
+						incQuality(item);
 					}
 				}
-			}
-		}
+			}						
+//////////////////////////////////////////
+		}		
 	}
-
 }
